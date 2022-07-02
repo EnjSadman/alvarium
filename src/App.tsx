d290cee779fb3fe0a7218fedb79acd1b7955710b
Roman Pavlenko <proman9973@gmail.com>
2022-07-02 10:40:26 +0300

    single card component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Card } from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card />
     </div>
   );
 }
